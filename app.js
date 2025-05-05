98e6d230fbbc32a883d33ffeb56dc5585e9c758f
FreshEffect15 <gossuinulises@gmail.com>
2025-05-05 12:11:24 -0300

    1

--- app.js
+++ app.js
@@ -5,24 +5,28 @@ const app = express();
 
 app.use(express.json());
 
+
 app.get('/', (req, res) => {
   res.send('Libros y autores');
 });
 
+
 const bookRoutes = require('./routes/books');
 const authorRoutes = require('./routes/authors');
+
 
 app.use('/api/books', bookRoutes);
 app.use('/api/authors', authorRoutes);
 
+
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Conectado efectivamente....');
 
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
-      console.log(` Servidor corriendo`);
+      console.log(`Servidor corriendo en el puerto ${PORT}`);
     });
   })
   .catch((err) => {
